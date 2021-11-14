1a66d6f8cbc350fe8865ab6357148698a708c184
cafir <ecafirhan@gmail.com>
2021-11-14 21:49:13 +0700

    google and jwt authentication

--- routes/holders.js
+++ routes/holders.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import { getHolders, createHolder, deleteHolder, updateHolder } from '../controllers/holders.js';
 
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 
 //read
-router.get('/', getHolders);
+router.get('/',  getHolders);
 //create
-router.post('/', createHolder);
+router.post('/', auth, createHolder);
 //update
-router.patch('/:id', updateHolder)
+router.patch('/:id', auth, updateHolder)
 //delete
-router.delete('/:id', deleteHolder)
+router.delete('/:id', auth, deleteHolder)
 
 export default router;
