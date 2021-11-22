712456abcec4691f3e0c66efc27ec7164d04df9e
cafir <ecafirhan@gmail.com>
2021-11-22 22:49:22 +0700

    get by id

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ const auth = async (req, res, next) => {
     
     try {
         const token = req.headers.authorization.split(" ")[1];
+        
         const isCustomAuth = token.length < 500;
 
         let decodedData;
